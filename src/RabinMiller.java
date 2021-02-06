21bdfaa378e42d6f46e4e7b6ca85d460dd608134
Aml-Abbas <aml.abbas@hotmail.com>
2021-02-06 11:05:41 +0100

    created a method to generate two primes according to the question 3

--- src/RabinMiller.java
+++ src/RabinMiller.java
@@ -56,6 +56,7 @@ public class RabinMiller {
     public BigInteger[] generateTwoPrimes(int length){
         BigInteger[] twoPrimes= new BigInteger[2];
 
+        
         return twoPrimes;
     }
 }
